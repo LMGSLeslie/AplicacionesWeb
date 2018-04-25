f5e2a77b50668e66275942047989ed77e260b636
Leslie Marjorie Gallegos Salazar <A01169525@itesm.mx>
2018-04-25 17:07:16 -0500

    Seleccion plantilla y persona

---
+++ ProyectoFinal/Scripts/functions.js
@@ -0,0 +1,21 @@
+﻿/*$('.list-group-item').click(function (e) {
+    e.preventDefault();
+
+    $that = $(this);
+
+    $that.parent().find('.list-group-item').removeClass('active');
+    $that.addClass('active');
+})*/
+
+$(function () {
+    console.log('ready');
+
+    $('.list-group li').click(function (e) {
+        e.preventDefault()
+
+        $that = $(this);
+
+        $('.list-group').find('li').removeClass('active');
+        $that.addClass('active');
+    });
+})

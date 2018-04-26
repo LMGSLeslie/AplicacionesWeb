c3ede3eab31a21513dfff901cb0623d5ac2dd4ff
Leslie Marjorie Gallegos Salazar <A01169525@itesm.mx>
2018-04-25 20:20:50 -0500

    Seleccion de persona y plantilla obligatorio

--- ProyectoFinal/Scripts/functions.js
+++ ProyectoFinal/Scripts/functions.js
@@ -1,15 +1,5 @@
-﻿/*$('.list-group-item').click(function (e) {
-    e.preventDefault();
-
-    $that = $(this);
-
-    $that.parent().find('.list-group-item').removeClass('active');
-    $that.addClass('active');
-})*/
-
+﻿
 $(function () {
-    console.log('ready');
-
     $('.list-group li').click(function (e) {
         e.preventDefault()
 
@@ -19,3 +9,18 @@ $(function () {
         $that.addClass('active');
     });
 })
+
+$(function alertaSiguiente() {
+    $('#Siguiente').on('click', function () {
+        console.log('Sientra');
+        var items = document.getElementsByClassName('list-group-item active');
+        if (items.length == 0) {
+            console.log('y tho');
+            window.alert('Por favor seleccione una opción');
+        } else {
+            console.log('wtf');
+            window.location.href = "../Pages/PantallaPrincipal.html";
+        }
+    })
+}
+)

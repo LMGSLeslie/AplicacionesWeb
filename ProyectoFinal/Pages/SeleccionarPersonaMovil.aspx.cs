bdf90cc2e9c05793a3c5532d4b5b6c735a945f82
Leslie Marjorie Gallegos Salazar <A01169525@itesm.mx>
2018-05-02 18:34:11 -0500

    :)

--- ProyectoFinal/Pages/SeleccionarPersonaMovil.aspx.cs
+++ ProyectoFinal/Pages/SeleccionarPersonaMovil.aspx.cs
@@ -10,7 +10,6 @@ namespace ProyectoFinal.Pages
 {
     public partial class SeleccionarPersonaMovil : System.Web.UI.Page
     {
-        SqlConnection conn = new SqlConnection("Server = DESKTOP-2F7769Q; Database = AplicacionesWeb; User Id = Rhapsodic; Password = wonderland01");
         private int PostBackCount
         {
             get
@@ -22,12 +21,20 @@ namespace ProyectoFinal.Pages
             }
             set { ViewState["PostBackCount"] = value; }
         }
+
+        SqlConnection conn = new SqlConnection("Server = DESKTOP-2F7769Q; Database = AplicacionesWeb; User Id = Rhapsodic; Password = wonderland01");
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (IsPostBack)
+            {
+                PostBackCount++;
+            }
             volver.Attributes.Add("onClick", "history.go" + "(-" + (PostBackCount + 1) + "); return false;");
-            siguiente.Attributes.Add("onClick", "history.go" + "(-" + (PostBackCount + 1) + "); return false;");
+            siguiente.Enabled = false;
+            ScriptManager.RegisterClientScriptBlock(Page, this.GetType(), "CallJS", "afterpostback();", true);
             conn.Open();
-            SqlCommand cmnd = new SqlCommand("SELECT persona_id , nombre+' ' + apellido as FULLNAME FROM Persona");
+            SqlCommand cmnd = new SqlCommand("SELECT persona_id, nombre+' ' + apellido as FULLNAME FROM Persona");
             cmnd.CommandType = System.Data.CommandType.Text;
             cmnd.Connection = conn;
 
@@ -54,6 +61,7 @@ namespace ProyectoFinal.Pages
             {
                 if (row.RowIndex == personas.SelectedIndex)
                 {
+                    siguiente.Enabled = true;
                     row.BackColor = System.Drawing.Color.LightGray;
                     row.ToolTip = string.Empty;
                 }
@@ -62,6 +70,52 @@ namespace ProyectoFinal.Pages
                     row.ToolTip = "Click to select this row.";
                 }
             }
+        }
+        protected void Seleccionar(object sender, EventArgs e)
+        {
+            string data = personas.SelectedRow.Cells[0].Text;
+            int id = int.Parse(data);
+
+            SqlCommand cmnd = new SqlCommand("INSERT INTO Relacion_huellas_persona VALUES(" + id + "," + int.Parse(Request.QueryString["PDPath"].ToString()) + "," + 1 + ")");
+            conn.Open();
+            cmnd.Connection = conn;
+            cmnd.ExecuteNonQuery();
+            cmnd = new SqlCommand("INSERT INTO Relacion_huellas_persona VALUES(" + id + "," + int.Parse(Request.QueryString["PIPath"].ToString()) + "," + 2 + ")");
+            cmnd.Connection = conn;
+            cmnd.ExecuteNonQuery();
+            cmnd = new SqlCommand("INSERT INTO Relacion_huellas_persona VALUES(" + id + "," + int.Parse(Request.QueryString["IDPath"].ToString()) + "," + 3 + ")");
+            cmnd.Connection = conn;
+            cmnd.ExecuteNonQuery();
+            cmnd = new SqlCommand("INSERT INTO Relacion_huellas_persona VALUES(" + id + "," + int.Parse(Request.QueryString["IIPath"].ToString()) + "," + 4 + ")");
+            cmnd.Connection = conn;
+            cmnd.ExecuteNonQuery();
+            cmnd = new SqlCommand("INSERT INTO Relacion_huellas_persona VALUES(" + id + "," + int.Parse(Request.QueryString["MDPath"].ToString()) + "," + 5 + ")");
+            cmnd.Connection = conn;
+            cmnd.ExecuteNonQuery();
+            cmnd = new SqlCommand("INSERT INTO Relacion_huellas_persona VALUES(" + id + "," + int.Parse(Request.QueryString["MIPath"].ToString()) + "," + 6 + ")");
+            cmnd.Connection = conn;
+            cmnd.ExecuteNonQuery();
+            cmnd = new SqlCommand("INSERT INTO Relacion_huellas_persona VALUES(" + id + "," + int.Parse(Request.QueryString["ADPath"].ToString()) + "," + 7 + ")");
+            cmnd.Connection = conn;
+            cmnd.ExecuteNonQuery();
+            cmnd = new SqlCommand("INSERT INTO Relacion_huellas_persona VALUES(" + id + "," + int.Parse(Request.QueryString["AIPath"].ToString()) + "," + 8 + ")");
+            cmnd.Connection = conn;
+            cmnd.ExecuteNonQuery();
+            cmnd = new SqlCommand("INSERT INTO Relacion_huellas_persona VALUES(" + id + "," + int.Parse(Request.QueryString["MñDPath"].ToString()) + "," + 9 + ")");
+            cmnd.Connection = conn;
+            cmnd.ExecuteNonQuery();
+            cmnd = new SqlCommand("INSERT INTO Relacion_huellas_persona VALUES(" + id + "," + int.Parse(Request.QueryString["MñIPath"].ToString()) + "," + 10 + ")");
+            cmnd.Connection = conn;
+            cmnd.ExecuteNonQuery();
+            cmnd = new SqlCommand("INSERT INTO Relacion_huellas_persona VALUES(" + id + "," + int.Parse(Request.QueryString["PlDPath"].ToString()) + "," + 11 + ")");
+            cmnd.Connection = conn;
+            cmnd.ExecuteNonQuery();
+            cmnd = new SqlCommand("INSERT INTO Relacion_huellas_persona VALUES(" + id + "," + int.Parse(Request.QueryString["PlIPath"].ToString()) + "," + 12 + ")");
+            cmnd.Connection = conn;
+            cmnd.ExecuteNonQuery();
+            conn.Close();
+            ClientScript.RegisterClientScriptBlock(this.GetType(), "alert", "<script type = 'text/javascript'> window.onload=function(){alert('Huellas asignadas, por favor regrese a la página anterior con el botón volver')};</script>");
+
         }
 
     }
